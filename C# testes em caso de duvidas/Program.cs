61a0ebe901ecedc9f5c0881eda096477efa4eee5
Mashiro <leonardosf9012@gmail.com>
2024-01-22 20:24:52 -0300

    Adicionar arquivos de projeto.

---
+++ C# testes em caso de duvidas/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("digite o primeiro número:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("digite o segundo número:");
+int b =Convert.ToInt32(Console.ReadLine());
+
+long resultado = (a^b);
+
+Console.WriteLine(resultado);
